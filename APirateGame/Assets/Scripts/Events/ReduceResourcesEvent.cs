c19e6b71a2e0b5acefc7de6076a720029eedd79d
avax94 <stefanovic.milan94@gmail.com>
2018-12-01 18:47:39 +0100

    Renamed plauge, added ReduceResourcesEvent

---
+++ APirateGame/Assets/Scripts/Events/ReduceResourcesEvent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Events
+{
+    public class ReduceResourcesEvent : ShipEvent
+    {
+        private uint foodToReduce;
+        private uint woodToReduce;
+
+        public ReduceResourcesEvent(uint foodToReduce, uint woodToReduce)
+        {
+            this.foodToReduce = foodToReduce;
+            this.woodToReduce = woodToReduce;
+        }
+
+
+        public override void ExecuteEventInternal(Ship ship)
+        {
+            ship.Inventory.ReduceResources(foodToReduce, woodToReduce);
+        }
+    }
+}
