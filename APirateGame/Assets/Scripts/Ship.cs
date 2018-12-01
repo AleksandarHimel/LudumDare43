26c8d1866a0b02af17fc43274302ba0640616b45
Andrija Jovanović <ja.andrija@gmail.com>
2018-12-01 15:08:52 +0100

    Ship-related abstract classes

---
+++ APirateGame/Assets/Scripts/Ship.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    public List<ShipPart> ShipParts { get; private set; }
+    public List<CrewMember> CrewMembers { get; private set; }
+
+	// Use this for initialization
+	void Start ()
+    {
+        ShipParts = new List<ShipPart>();
+        //ShipParts.Add(...)
+
+        CrewMembers = new List<CrewMember>();
+        // CrewMembers.Add(...)
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public bool IsDestroyed()
+    {
+        return false;
+    }
+}
---
+++ APirateGame/Assets/Scripts/ShipPart.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ShipPart : MonoBehaviour {
+
+    public uint MaxNumberOfCrewMembers { get; protected set; }
+
+    public List<CrewMember> PresentCrewMembers { get; protected set; }
+
+    public uint Health { get; protected set; }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
