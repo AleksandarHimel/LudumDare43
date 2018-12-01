a7ae3ed0e8240deb373c404e7d609e0d553b94b9
Andrija Jovanović <ja.andrija@outlook.com>
2018-12-01 16:49:06 +0100

    Fix ShipPart syntax errors

--- APirateGame/Assets/Scripts/ShipParts/Cannon.cs
+++ APirateGame/Assets/Scripts/ShipParts/Cannon.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Cannon : ShipPart
 {
+    public Cannon(Ship parentShip) : base(parentShip) { }
+
 	// Use this for initialization
 	void Start ()
     {
         MaxNumberOfCrewMembers = 1;
         MaxHealth = 2;
+        Health = MaxHealth;
 	}
 	
 	// Update is called once per frame
--- APirateGame/Assets/Scripts/ShipParts/Kitchen.cs
+++ APirateGame/Assets/Scripts/ShipParts/Kitchen.cs
@@ -2,20 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hull : ShipPart
-{
-    public Hull(Ship parentShip) : base(parentShip) { }
+public class Kitchen : ShipPart {
+
+    public Kitchen(Ship parentShip) : base(parentShip) { }
 
 	// Use this for initialization
 	void Start ()
     {
-        MaxNumberOfCrewMembers = 0;
+        MaxNumberOfCrewMembers = 10;
         MaxHealth = 2;
         Health = MaxHealth;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 }
