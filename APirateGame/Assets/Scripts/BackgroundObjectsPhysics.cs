103bd02d2cc95e1254a78399aef7375d7b4d9eb7
aleksandar-z-zivanovic <45500914+aleksandar-z-zivanovic@users.noreply.github.com>
2018-12-02 17:08:04 +0100

    stuff for the main scene
    stuff for the main scene

---
+++ APirateGame/Assets/Scripts/BackgroundObjectsPhysics.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundObjectsPhysics : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    public float Xspeed;
+    public float Amplitude;
+    public float Frequency;
+    public float resetX;
+    private Vector2 StartingPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Xspeed = -0.7f;
+        rb.velocity = new Vector2(Xspeed, 0.0f);
+        Amplitude = 0.3f;
+        Frequency = 1f;
+        StartingPosition = rb.position;
+        resetX = 9f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 newposition = new Vector2(rb.position.x + Xspeed * Time.deltaTime, StartingPosition.y + Amplitude * Mathf.Sin(Time.fixedTime * Frequency));
+
+        if (newposition.x <= -resetX)
+        {
+            newposition.x = StartingPosition.x;
+        }
+
+        rb.MovePosition(newposition);
+
+
+    }
+}
