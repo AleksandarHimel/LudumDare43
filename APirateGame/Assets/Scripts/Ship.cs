ef962e7ba44dd16c2233c6efd4bbac3dc075ff4b
flcx <miodrag.radulovic@gmail.com>
2018-12-04 01:26:51 +0100

    Fix stats calculation not to take into account dead people

--- APirateGame/Assets/Scripts/Ship.cs
+++ APirateGame/Assets/Scripts/Ship.cs
@@ -172,14 +172,14 @@ public class Ship : MonoBehaviour, IPointerClickHandler
         int foodConsumption = 0;
         
         // Sum up food consumption
-        foodConsumption = foodConsumption + CrewMembers.Select(crewMember => crewMember.GetResourceConcuption()).Sum();
+        foodConsumption = foodConsumption + AliveCrewMembers.Select(crewMember => crewMember.GetResourceConcuption()).Sum();
 
         return foodConsumption;
     }
 
     public int CalculateBoatSpeed()
     {
-        double sailingFactor = CrewMembers
+        double sailingFactor = AliveCrewMembers
                                 .Where(crewMember => crewMember!= null && crewMember.CurrentShipPart != null && crewMember.CurrentShipPart is Sails)
                                 .Select(crewMember => crewMember.GetAttribute("Sailing") == null ? 1.0 : (double) crewMember.GetAttribute("Sailing").AttributeValue)
                                 .DefaultIfEmpty(1.0)
@@ -197,7 +197,7 @@ public class Ship : MonoBehaviour, IPointerClickHandler
         */
 
         double rowingSpeedIncrement =
-            CrewMembers
+            AliveCrewMembers
             .Where(crewMember => crewMember.CurrentShipPart is Oars)
             // TODO: v-milast check if rowing is valid
             .Select(crewMember => crewMember.GetAttribute("Rowing"))
