12fc686ca279416c9de40237b5d527cf71e69116
flcx <miodrag.radulovic@gmail.com>
2018-12-04 03:00:39 +0100

    skip load

--- APirateGame/Assets/Scripts/TimerLoad.cs
+++ APirateGame/Assets/Scripts/TimerLoad.cs
@@ -12,7 +12,10 @@ public class TimerLoad : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            LoadNext();
+        }
 	}
 
     void LoadNext()
